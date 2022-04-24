609f0c1ca33b38c6975bd47be035366020d37d88
satoruando <ando.str15@gmail.com>
2022-04-24 05:35:18 +0000

    migrate変更

--- db/migrate/20220406055505_create_nices.rb
+++ db/migrate/20220406055505_create_nices.rb
@@ -2,7 +2,7 @@ class CreateNices < ActiveRecord::Migration[6.1]
   def change
     create_table :nices do |t|
       t.integer :member_id, null: false
-      t.integer :video_id,
+      t.integer :video_id
 
       t.timestamps
     end
